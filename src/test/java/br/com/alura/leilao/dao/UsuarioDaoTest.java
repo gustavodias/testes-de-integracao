bab9732c147e1741ce712754469002a8a878de18
gustavodias <gustavoemersonfd@gmail.com>
2021-12-14 19:46:44 -0300

    Refactoring - Aplicando boas praticas.

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -2,40 +2,48 @@ package br.com.alura.leilao.dao;
 
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.util.JPAUtil;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 
-public class UsuarioDaoTest {
+class UsuarioDaoTest {
 
     private UsuarioDao dao;
+    private EntityManager em;
 
-    @Test
-    public void deveriaEncontrarUsuarioCadastrado() {
-        EntityManager em =JPAUtil.getEntityManager();
+    @BeforeEach
+    public void beforeEach(){
+       this.em = JPAUtil.getEntityManager();
         this.dao = new UsuarioDao(em);
-
-        Usuario usuario = new Usuario("fulano", "funalo@email.com", "12345678");
         em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
+    }
 
-        Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
-        Assert.assertNotNull(usuarioEncontrado);
+    @AfterEach
+    public void afterEach(){
+        this.em.getTransaction().rollback();
     }
 
     @Test
-    public void naoDeveriaEncontrarUsuarioNaoCadastrado() {
-        EntityManager em =JPAUtil.getEntityManager();
-        this.dao = new UsuarioDao(em);
+    void deveriaEncontrarUsuarioCadastrado() {
+        Usuario usuario = criarUsuario();
+        Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
+        Assertions.assertNotNull(usuarioEncontrado);
+    }
 
+    @Test
+    void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+        criarUsuario();
+        Assertions.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("Beltrano"));
+    }
+
+
+    public Usuario criarUsuario(){
         Usuario usuario = new Usuario("fulano", "funalo@email.com", "12345678");
-        em.getTransaction().begin();
         em.persist(usuario);
-        em.getTransaction().commit();
-
-        Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("Beltrano"));
+        return usuario;
     }
 }
