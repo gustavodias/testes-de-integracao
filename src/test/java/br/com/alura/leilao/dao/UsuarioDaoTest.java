96d9cb09cce4fa3de6d01f9a2fa37535f1380b72
gustavodias <gustavoemersonfd@gmail.com>
2021-12-14 19:20:40 -0300

    Teste para verificar se não encontra um usuario nao cadastrado.

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -6,13 +6,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 public class UsuarioDaoTest {
 
     private UsuarioDao dao;
 
     @Test
-    public void buscarPorUsername() {
+    public void deveriaEncontrarUsuarioCadastrado() {
         EntityManager em =JPAUtil.getEntityManager();
         this.dao = new UsuarioDao(em);
 
@@ -23,5 +24,18 @@ public class UsuarioDaoTest {
 
         Usuario usuarioEncontrado = this.dao.buscarPorUsername(usuario.getNome());
         Assert.assertNotNull(usuarioEncontrado);
+    }
+
+    @Test
+    public void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+        EntityManager em =JPAUtil.getEntityManager();
+        this.dao = new UsuarioDao(em);
+
+        Usuario usuario = new Usuario("fulano", "funalo@email.com", "12345678");
+        em.getTransaction().begin();
+        em.persist(usuario);
+        em.getTransaction().commit();
+
+        Assert.assertThrows(NoResultException.class, () -> this.dao.buscarPorUsername("Beltrano"));
     }
 }
